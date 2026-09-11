718c3cef73ca81bfe50365c71306aab209f9cea7
Adrian Saint <adrian@saintmentalist.com>
2026-09-11 09:41:39 +0600

    Remove white text titles from video cards in testimonials grid

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
    ========================================================================== */
 
 import { CITIES_DATA } from './data/cities.js?v=20260821_v3';
-import { TESTIMONIALS_DATA, VIDEO_TESTIMONIALS_DATA } from './data/testimonials.js?v=20260911_v16';
+import { TESTIMONIALS_DATA, VIDEO_TESTIMONIALS_DATA } from './data/testimonials.js?v=20260911_v17';
 import { GENERAL_FAQS } from './data/faqs.js?v=20260908_v65';
 import { renderCityPage } from './components/cityRenderer.js?v=20260821_v3';
 import { setupBookingModal } from './components/bookingModal.js?v=20260821_v3';
